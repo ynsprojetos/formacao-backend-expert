32dfa7cc487ae8ccadd2482d0c2915cce516e0ca
Yuri Silva <yuri.silva@cialne.com.br>
2024-09-17 20:08:25 -0300

    feat: adiciona serviço de usuarios

---
+++ user-service-api/src/test/java/br/com/yurisilva/user_service_api/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yurisilva.user_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
